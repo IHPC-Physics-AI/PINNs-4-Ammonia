f7e4f38b346174ed4fd052bd3e4ab46f38c3d4f5
ryanangbh <RANG016@e.ntu.edu.sg>
2025-06-30 21:36:35 +0800

    Create Vel_gasmixture.py

---
+++ jax_data_generation/helper_functions/Vel_gasmixture.py
@@ -0,0 +1,13 @@
+import jax
+import jax.numpy as jnp
+
+from Reac_class import *
+from Oper_class import *
+
+def Vel_gasmixture(reac, oper):
+
+  SV = oper.SV0 * (oper.T0 / 273.15) / (oper.P0 / 101325) # Space Velocity at Inlet
+
+  return SV / reac.epsilon_b
+
+jax.jit(Vel_gasmixture, static_argnums=(0,1))
