1cc286e0d3b80226dd6f596f390e9aa8b06fa64d
ryanangbh <RANG016@e.ntu.edu.sg>
2025-06-30 21:44:36 +0800

    Update Vel_gasmixture.py

--- jax_data_generation/helper_functions/Vel_gasmixture.py
+++ jax_data_generation/helper_functions/Vel_gasmixture.py
@@ -4,6 +4,8 @@ import jax.numpy as jnp
 from Reac_class import *
 from Oper_class import *
 
+jax.config.update("jax_enable_x64", True)
+
 def Vel_gasmixture(reac, oper):
 
   SV = oper.SV0 * (oper.T0 / 273.15) / (oper.P0 / 101325) # Space Velocity at Inlet
