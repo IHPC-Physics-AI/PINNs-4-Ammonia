85f2a75e4d41165234d283d0f6a8431d1d2f5f5e
ryanangbh <RANG016@e.ntu.edu.sg>
2025-06-30 21:31:22 +0800

    Create Molecular_weight_i.py

---
+++ jax_data_generation/helper_functions/Molecular_weight_i.py
@@ -0,0 +1,10 @@
+import jax
+import jax.numpy as jnp
+
+def Molecular_weight_i():
+  Wi = [17.0305E-3,     #N H3
+        2.01588E-3,     # H2
+        28.0134E-3,     # N2
+        39.9480E-3]     # Ar
+
+  return jnp.asarray(Wi)
